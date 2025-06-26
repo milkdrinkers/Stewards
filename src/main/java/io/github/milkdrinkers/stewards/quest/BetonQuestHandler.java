972a0499bd0b0983dfcbedded3df91984c56a211
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2025-06-27 01:35:26 +0200

    fix: register all condition factories

--- src/main/java/io/github/milkdrinkers/stewards/quest/BetonQuestHandler.java
+++ src/main/java/io/github/milkdrinkers/stewards/quest/BetonQuestHandler.java
@@ -2,6 +2,8 @@ package io.github.milkdrinkers.stewards.quest;
 
 import io.github.milkdrinkers.stewards.Reloadable;
 import io.github.milkdrinkers.stewards.Stewards;
+import io.github.milkdrinkers.stewards.quest.condition.CanAffordArchitectConditionFactory;
+import io.github.milkdrinkers.stewards.quest.condition.HasArchitectConditionFactory;
 import io.github.milkdrinkers.stewards.quest.condition.HasTownConditionFactory;
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
@@ -18,7 +20,9 @@ public class BetonQuestHandler implements Reloadable {
         final BetonQuestLoggerFactory loggerFactory = BetonQuest.getInstance().getLoggerFactory();
         final PrimaryServerThreadData data = new PrimaryServerThreadData(plugin.getServer(), plugin.getServer().getScheduler(), plugin);
 
-        BetonQuest.getInstance().getQuestRegistries().getConditionTypes().register("stewardhastown", new HasTownConditionFactory(loggerFactory, data));
+        BetonQuest.getInstance().getQuestRegistries().getConditionTypes().register("stewardshastown", new HasTownConditionFactory(loggerFactory, data));
+        BetonQuest.getInstance().getQuestRegistries().getConditionTypes().register("stewardshasarchitect", new HasArchitectConditionFactory(loggerFactory, data));
+        BetonQuest.getInstance().getQuestRegistries().getConditionTypes().register("stewardscanaffordarchitect", new CanAffordArchitectConditionFactory(loggerFactory, data));
     }
 
     @Override
