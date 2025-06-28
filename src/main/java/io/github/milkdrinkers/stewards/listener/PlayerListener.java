59cae6d6736e7dd8dd605878175c39030ab8b88b
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2025-06-28 22:33:49 +0200

    fix: respawn on cross dimension teleporting

--- src/main/java/io/github/milkdrinkers/stewards/listener/PlayerListener.java
+++ src/main/java/io/github/milkdrinkers/stewards/listener/PlayerListener.java
@@ -1,12 +1,14 @@
 package io.github.milkdrinkers.stewards.listener;
 
 import com.palmergames.bukkit.towny.TownyAPI;
+import io.github.milkdrinkers.stewards.Stewards;
 import io.github.milkdrinkers.stewards.steward.Steward;
 import io.github.milkdrinkers.stewards.steward.StewardLookup;
 import io.github.milkdrinkers.stewards.towny.TownMetaData;
 import io.github.milkdrinkers.stewards.trait.ArchitectTrait;
 import io.github.milkdrinkers.stewards.trait.StewardTrait;
 import io.github.milkdrinkers.stewards.utility.Cfg;
+import io.github.milkdrinkers.threadutil.Scheduler;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -20,7 +22,13 @@ public class PlayerListener implements Listener {
     public void onPlayerLeave(PlayerQuitEvent e) {
         if (StewardLookup.get().isPlayerFollowed(e.getPlayer())) {
             Steward steward = StewardLookup.get().getStewardFollowingPlayer(e.getPlayer());
-            if (!steward.getSettler().getNpc().getOrAddTrait(StewardTrait.class).isHired()) steward.getSettler().delete();
+
+            if (!steward.getSettler().getNpc().hasTrait(ArchitectTrait.class)) {
+                if (!steward.getSettler().getNpc().getOrAddTrait(StewardTrait.class).isHired()) {
+                    steward.getSettler().delete();
+                    return;
+                }
+            }
 
             StewardTrait trait = steward.getSettler().getNpc().getOrAddTrait(StewardTrait.class);
 
@@ -36,9 +44,15 @@ public class PlayerListener implements Listener {
             Steward steward = StewardLookup.get().getStewardFollowingPlayer(e.getPlayer());
             if (!steward.getSettler().getNpc().getOrAddTrait(StewardTrait.class).isHired()) {
                 if (steward.getSettler().getNpc().hasTrait(ArchitectTrait.class)) {
-                    steward.getSettler().getNpc().teleport(e.getTo(), PlayerTeleportEvent.TeleportCause.PLUGIN);
-                    steward.stopFollowing(e.getPlayer());
-                    steward.startFollowing(e.getPlayer());
+                    steward.getSettler().despawn();
+
+                    Scheduler.sync(() -> {
+                        steward.stopFollowing(e.getPlayer());
+                        steward.getSettler().despawn();
+                    }).delay(5)
+                        .sync(() -> steward.getSettler().spawn(e.getTo()))
+                        .delay(5).sync(() -> steward.startFollowing(e.getPlayer()))
+                        .execute();
                 } else {
                     steward.stopFollowing(e.getPlayer());
                 }
