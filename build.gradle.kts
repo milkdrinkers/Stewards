ba7ddd8b8e6719b18388ffd063ffae46e565c8e9
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2025-07-12 18:21:24 -0400

    fix: stewards ports patch

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
         exclude("com.comphenix.packetwrapper", "PacketWrapper")
         exclude("de.themoep", "minedown-adventure")
     }
-    compileOnly(files("libs/AlathraPorts-1.0.1.jar"))
+    compileOnly(files("libs/AlathraPorts-1.0.2.jar"))
 
     // Testing - Core
     testImplementation(libs.annotations)
